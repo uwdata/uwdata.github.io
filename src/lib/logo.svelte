401a09353451e54172748b5b1f56c72d177e9f49
jheer <jeffrey.heer@gmail.com>
2024-04-25 15:19:12 -0700

    chore: Run prettier.

--- src/lib/logo.svelte
+++ src/lib/logo.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 	import { forceSimulation, forceManyBody, forceLink } from 'd3-force';
 	import { base } from '$app/paths';
 	const data = [
@@ -40,11 +40,11 @@
 	onMount(() => {
 		// initialize simulation nodes
 		const pointer = { fx: 10000, fy: 10000 };
-		const movable = data.map(d => ({
+		const movable = data.map((d) => ({
 			x: Math.round(d[0] * 0.5 * cell),
 			y: Math.round(d[1] * 0.5 * -cell)
 		}));
-		const anchors = movable.map(d => ({ fx: d.x, fy: d.y }));
+		const anchors = movable.map((d) => ({ fx: d.x, fy: d.y }));
 		const nodes = [pointer, ...movable, ...anchors];
 
 		// svg elements to position
@@ -54,16 +54,13 @@
 		const chargeForce = forceManyBody()
 			.distanceMin(10)
 			.distanceMax(40)
-			.strength((d, i) => i ? 0 : power);
+			.strength((d, i) => (i ? 0 : power));
 
 		// link movable nodes to anchor nodes
-		const links = Array.from(
-			{ length: N },
-			(_, i) => ({ source: i + 1, target: N + i + 1 })
-		);
+		const links = Array.from({ length: N }, (_, i) => ({ source: i + 1, target: N + i + 1 }));
 		const linkForce = forceLink(links).strength(0.3).distance(0);
 
-    // instantiate force simulation and position updates
+		// instantiate force simulation and position updates
 		const force = forceSimulation(nodes)
 			.force('charge', chargeForce)
 			.force('link', linkForce)
@@ -81,8 +78,8 @@
 			force.alpha(1).restart();
 		}
 
-    // respond to pointer movement
-		svg.addEventListener('pointermove', event => {
+		// respond to pointer movement
+		svg.addEventListener('pointermove', (event) => {
 			let point = svg.createSVGPoint();
 			point.x = event.clientX;
 			point.y = event.clientY;
@@ -90,7 +87,7 @@
 			updatePointer(point.x - tx, point.y - ty);
 		});
 
-    // respond to pointer going away
+		// respond to pointer going away
 		svg.addEventListener('pointerout', () => {
 			updatePointer(10000, 10000);
 		});
@@ -98,46 +95,46 @@
 </script>
 
 <div>
-<a href={`/${base}`}>
-	<svg width={w} height={h} bind:this={svg}>
-	  <g transform={`translate(${tx}, ${ty})`}>
-		  <g fill-opacity={faded}>
-		  {#each data as node}
-				<rect
-					class={`logo-${letter[node[2]]}`}
-					x={Math.round(node[0] * 0.5 * cell)}
-					y={Math.round(node[1] * 0.5 * -cell)}
-					height={cell - pad}
-					width={cell - pad}
-				/>
-			{/each}
+	<a href={`/${base}`}>
+		<svg width={w} height={h} bind:this={svg}>
+			<g transform={`translate(${tx}, ${ty})`}>
+				<g fill-opacity={faded}>
+					{#each data as node}
+						<rect
+							class={`logo-${letter[node[2]]}`}
+							x={Math.round(node[0] * 0.5 * cell)}
+							y={Math.round(node[1] * 0.5 * -cell)}
+							height={cell - pad}
+							width={cell - pad}
+						/>
+					{/each}
+				</g>
+				<g class="active">
+					{#each data as node}
+						<rect
+							class={`logo-${letter[node[2]]}`}
+							x={Math.round(node[0] * 0.5 * cell)}
+							y={Math.round(node[1] * 0.5 * -cell)}
+							height={cell - pad}
+							width={cell - pad}
+						/>
+					{/each}
+				</g>
 			</g>
-			<g class="active">
-			{#each data as node}
-				<rect
-					class={`logo-${letter[node[2]]}`}
-					x={Math.round(node[0] * 0.5 * cell)}
-					y={Math.round(node[1] * 0.5 * -cell)}
-					height={cell - pad}
-					width={cell - pad}
-				/>
-			{/each}
-			</g>
-		</g>
-	</svg>
-</a>
+		</svg>
+	</a>
 </div>
 
 <style>
-div {
-	height: 65px;
-}
-a {
-	position: absolute;
-	left: -5px;
-  top: -9px;
-  width: 99px;
-  height: 85px;
-  overflow: hidden;
-}
+	div {
+		height: 65px;
+	}
+	a {
+		position: absolute;
+		left: -5px;
+		top: -9px;
+		width: 99px;
+		height: 85px;
+		overflow: hidden;
+	}
 </style>
