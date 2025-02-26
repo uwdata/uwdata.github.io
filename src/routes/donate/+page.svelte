8cd909f2533246310a0f678efebc8a377c2f66a6
jheer <jeffrey.heer@gmail.com>
2025-02-26 14:09:18 -0800

    feat: Add donate page.

---
+++ src/routes/donate/+page.svelte
@@ -0,0 +1,35 @@
+<svelte:head>
+  <title>Donate to IDL</title>
+</svelte:head>
+
+<div class="lead">Support the Interactive Data Lab</div>
+
+<div class="md:hidden">
+  <p>
+    Join us to support cutting-edge research, student development, and impactful open source software. <a class="font-semibold" href="https://www.washington.edu/giving/make-a-gift/?page=make&code=FND-152008">Make a gift</a> to the Interactive Data Lab!
+  </p>
+  <div class="text-center pt-6 pb-8">
+    <a
+      href="https://www.washington.edu/giving/make-a-gift/?page=make&code=FND-152008"
+      class="rounded-md bg-violet-800 px-3.5 py-2.5 text-2xl font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-violet-100  focus-visible:outline-violet">Donate Now</a>
+  </div>
+</div>
+
+<div class="hidden md:flex flex py-5 items-center">
+  <div class="w-48 flex-none text-center">
+    <a
+      href="https://www.washington.edu/giving/make-a-gift/?page=make&code=FND-152008"
+      class="rounded-md bg-violet-800 px-3.5 py-2.5 text-lg font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-violet-100  focus-visible:outline-violet">Donate Now</a>
+  </div>
+  <div class="flex-auto pr-10">
+    Join us to support cutting-edge research, successful students, and impactful open source software. <a class="font-semibold" href="https://www.washington.edu/giving/make-a-gift/?page=make&code=FND-152008">Make a gift</a> to the Interactive Data Lab!
+  </div>
+</div>
+
+<p>
+  In addition to our <a class="font-semibold" href="papers">research output</a> and <a class="font-semibold" href="https://idl.uw.edu/visualization-curriculum/">educational curricula</a>, the Interactive Data Lab develops and contributes to popular <a class="font-semibold" href="https://github.com/uwdata/">open source software</a> projects including <a class="font-semibold" href="https://vega.github.io/vega-lite">Vega-Lite</a>, <a class="font-semibold" href="https://altair-viz.github.io/">Vega-Altair</a>, <a class="font-semibold" href="https://d3js.org">D3.js</a>, <a class="font-semibold" href="https://idl.uw.edu/arquero">Arquero</a>, and <a class="font-semibold" href="https://idl.uw.edu/mosaic">Mosaic</a>. Your gift helps us enhance people's ability to understand and communicate data, including support for student researchers and open source development.
+</p>
+
+<p>
+  Donations directly support the Interactive Data Lab and are processed by the UW Foundation (tax id 94-3079432). Your gift may be tax deductible as charitable contributions for U.S. federal tax purposes as allowed by IRS regulations. For more, see the <a class="font-semibold" href="https://www.washington.edu/giving/contacts/faq">UW Giving Frequently Asked Questions</a>.
+</p>
