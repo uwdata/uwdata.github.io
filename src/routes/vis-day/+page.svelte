9def6e9518f16ab01aa5a92c5e13cfbea82ec4e4
jheer <jeffrey.heer@gmail.com>
2025-05-02 13:39:57 -0700

    feat: Update Vis Day schedule.

--- src/routes/vis-day/+page.svelte
+++ src/routes/vis-day/+page.svelte
@@ -1,10 +1,226 @@
-<script lang="ts">
-  import Markdown from '$lib/markdown.svelte';
-  import content from './vis-day.md?raw';
-</script>
-
 <svelte:head>
-  <title>Northwest Visualization Day</title>
+  <title>Seattle Visualization Day</title>
 </svelte:head>
 
-<Markdown {content} />
+<article class="markdown">
+  <h2>Seattle Visualization Day</h2>
+
+  <p>
+  The first-ever <strong>Seattle Visualization Day</strong> will be on <strong>Friday May 9, 2025</strong> at the University of Washington in Seattle.
+  This event is intended to bring together visualization researchers and practitioners across academia, industry, and government in the Pacific Northwest region.
+  The event is an opportunity to discuss the state-of-the-art in visualization, share projects, and build local community.
+  </p>
+
+  <h2>Schedule</h2>
+
+  <table>
+  <thead>
+    <tr>
+      <th>Time</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>8:00am</td>
+      <td>
+        <strong>Breakfast</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>9:00am</td>
+      <td>
+        <strong>Introduction</strong><br/>
+        Jeffrey Heer (University of Washington)
+      </td>
+    </tr>
+    <tr>
+      <td>9:15am</td>
+      <td>
+        <strong>Keynote Talk</strong><br/>
+        Sheelagh Carpendale (Simon Fraser University)<br/>
+        <em>What Do We Mean by Data Empowerment?</em>
+      </td>
+    </tr>
+    <tr>
+      <td>9:45am</td>
+      <td>
+        <strong>Short Talks: Visualization Design</strong><br/>
+        Session Chair: Hyeok Kim (University of Washington)<br/>
+        <ul>
+          <li>Lyn Bartram (Simon Fraser University), <em>Beyond Data: Affective Visualization</em></li>
+          <li>Robert Kosara, <em>It’s About Time: A Tour of Time Axes in Data Visualization</em></li>
+          <li>Amber Thomas (USA Facts), <em>Big Budget, Small Screen: Rethinking How We Visualize Federal Spending</em></li>
+          <li>Kanit Wongsuphasawat (Databricks), <em>Visualization Grammar, Beyond Just Charts</em></li>
+        </ul>
+      </td>
+    </tr>
+    <tr>
+      <td>10:40am</td>
+      <td>
+        <strong>Coffee Break</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>11:00am</td>
+      <td>
+        <strong>Panel Discussion</strong><br/>
+        Moderator: Leilani Battle (University of Washington)<br/>
+        <ul>
+          <li>Jeffrey Heer (University of Washington)</li>
+          <li>Tamara Munzner (University of British Columbia)</li>
+          <li>Maureen Stone (University of Washington)</li>
+          <li>Justin Talbot (Databricks)</li>
+          <li>Amber Thomas (USA Facts)</li>
+        </ul>
+      </td>
+    </tr>
+    <tr>
+      <td>12:00pm</td>
+      <td>
+        <strong>Lunch &amp; Poster Session</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>1:30pm</td>
+      <td>
+        <strong>Short Talks: Visualization &amp; AI</strong><br/>
+        Session Chair: Will Huichen Wang (University of Washington)<br/>
+        <ul>
+          <li>Chenglong Wang (Microsoft Research), <em>Data Formulator: Create Rich Visualizations Iteratively, with AI</em></li>
+          <li>Bill Howe (University of Washington), <em>Assessing Fidelity for AI-Assisted Visualization</em></li>
+          <li>Alper Sarikaya (Microsoft), <em>Intent-driven
+          Visualization Design</em></li>
+          <li>Vidya Setlur (Tableau), <em>Bridging Semantics and Sensemaking: Designing Intelligent Tools for Visual Analytics</em></li>
+        </ul>
+      </td>
+    </tr>
+    <tr>
+      <td>2:30pm</td>
+      <td>
+        <strong>Coffee Break</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>2:50pm</td>
+      <td>
+        <strong>Keynote Talk</strong><br/>
+        Ariel Rokem (University of Washington)<br/>
+        <em>The Challenges of Visualizing Brain Connections</em>
+      </td>
+    </tr>
+    <tr>
+      <td>3:20pm</td>
+      <td>
+        <strong>Birds of a Feather Discussion</strong><br/>
+        Moderator: Maureen Stone (University of Washington)
+      </td>
+    </tr>
+    <tr>
+      <td>4:20pm</td>
+      <td>
+        <strong>Closing</strong><br/>
+        Jeffrey Heer (University of Washington)
+      </td>
+    </tr>
+    <tr>
+      <td>4:30pm</td>
+      <td>
+        <strong>Event Ends</strong><br/>
+        Attendees are invited to socialize!
+      </td>
+    </tr>
+  </tbody>
+  </table>
+
+  <h2>Registration</h2>
+
+  <p>
+  Registration is required. To RSVP, please submit the
+  <span style="padding: 2px 4px; border: 1px solid purple;"><strong>
+    <a href="https://forms.gle/yP6Bw1oP5wfDxpZ18">Northwest Visualization Day RSVP Form</a>
+  </strong></span>
+  </p>
+
+  <!-- <p>
+  We plan to include everyone who submits the registration form.
+  However, in the unlikely event that we receive more sign-ups than we can accommodate, we may have to limit the number of attendees.
+  We will contact registrants well in advance should this become necessary.
+  </p> -->
+
+  <p>
+  We will share further updates, event details, and schedule information with registered participants as the date nears.
+  </p>
+
+  <h2>Organizers</h2>
+
+  <p>
+    Questions? Please contact the event organizers at <strong>seattle-vis@cs.washington.edu</strong>.
+  </p>
+  <p>
+    Leilani Battle, Maureen Stone, Jeffrey Heer &ndash; <em>University of Washington</em>
+  </p>
+
+  <h2>Support</h2>
+
+  <p>
+    Interested in supporting the Interactive Data Lab?
+    <a href="/donate">Please consider making a donation</a>.
+  </p>
+
+  <h2>Venue</h2>
+
+  <p>
+  Seattle Visualization Day will be held at the
+  <a href="https://www.cs.washington.edu/visitors/getting_here">Bill and Melinda Gates Center</a> at the University of Washington, Seattle.
+  Big thanks to the <a href="https://www.cs.washington.edu/">Paul G. Allen School of Computer Science & Engineering</a>!
+
+  </p>
+
+  <p>
+  <a href="https://www.cs.washington.edu/visitors/getting_here">
+  <img style="max-width: 500px;"
+    alt="Bill and Melinda Gates Center for Computer Science & Engineering"
+    src="https://seclab.cs.washington.edu/wp-content/uploads/2019/03/GatesCenter.jpg"
+  />
+  </a>
+  </p>
+</article>
+
+<style>
+  table {
+    border-spacing: 1em;
+    border-collapse: collapse;
+  }
+
+  th, td {
+    padding: 0.5em;
+    border: 1px solid #ccc;
+  }
+
+  th {
+    text-align: left;
+  }
+
+  th:first-child {
+    text-align: center;
+  }
+
+  td:first-child {
+    text-align: right;
+    vertical-align: top;
+  }
+
+  ul {
+    list-style-type: circle;
+    margin: 1em;
+  }
+
+  a {
+    color: #6a2eb3;
+  }
+
+  a:hover {
+    color: #8a5ed3;
+  }
+</style>
